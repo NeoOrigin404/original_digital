5318f43a0a55be2180e482ed5a72c772a76c56f9
Julien PICART <julienpicart01@gmail.com>
2025-04-06 15:56:11 +0200

    Movie details : in progress

--- client/src/pages/MovieDetail.tsx
+++ client/src/pages/MovieDetail.tsx
@@ -19,51 +19,47 @@ export default function MovieDetail() {
     .slice(0, 12);
 
   return (
-    <>
-      <h1>Original digitals</h1>
-      <img className="landscape" src={movieId.landscape_image} alt="" />
-      <h2 className="movie-titleDetail">{movieId.title}</h2>
-      <div className="container-movies">
-        <div className="landscape-information">
-          <div className="information-wrap">
-            <div className="information-left">
-              <p>
-                {movieId.release_year} {movieId.duration}
-              </p>
-              <p>{movieId.genres}</p>
-            </div>
-            <div className="bio">
-              <p>synopsis : {movieId.synopsis.slice(0, 50)}...</p>
-              <details>
-                <summary>Voir plus</summary>
-                <p>{movieId.synopsis.substring(50)}</p>
-              </details>
-              <FavoriteButton id={movieId.id} />
-            </div>
-            <div className="information-right">
-              <p>casting : {movieId.casting}</p>
-              <p>production : {movieId.production}</p>
-            </div>
-          </div>
+    <section className="movie-details">
+      <div
+        className="details"
+        style={{
+          backgroundImage: `linear-gradient(rgba(11, 11, 11, 0.2), rgba(11, 11, 11, 0.2)), url(${movieId.landscape_image})`,
+        }}
+      >
+        <h2 className="movie-titleDetail">{movieId.title}</h2>
+        <p>
+          {movieId.genres} . {movieId.duration} . {movieId.release_year}
+        </p>
+        <div className="content-details">
+          <span className="badge-4k" aria-label="4K" role="img" />
+          <span className="dolby-vision" aria-label="Dolby Vision" role="img" />
+          <span className="dolby-atmos" aria-label="Dolby Atmos" role="img" />
+          <span className="cc" aria-label="Sous-titres codés" role="img" />
+          <span className="ad" aria-label="Audiodescription" role="img" />
+          <span
+            className="sdh"
+            aria-label="Sous-titrage pour les sourds et malentendants"
+            role="img"
+          />
         </div>
-        <iframe
-          className="short-movie"
-          width="100%"
-          height="auto"
-          src={movieId.trailer}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        />
-        <h3 className="same-genre">Vous pourriez aimer aussi...</h3>
-        <section className="movie-container">
-          {sameGenre.map((movie) => (
-            <MovieCards key={movie.id} movie={movie} />
-          ))}
-        </section>
       </div>
-    </>
+      <iframe
+        className="short-movie"
+        width="100%"
+        height="auto"
+        src={movieId.trailer}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      />
+      <h3 className="same-genre">Vous pourriez aimer aussi...</h3>
+      <section className="movie-container">
+        {sameGenre.map((movie) => (
+          <MovieCards key={movie.id} movie={movie} />
+        ))}
+      </section>
+    </section>
   );
 }
